bb0af2efe2435716e05cc18d3a5bddf2b613ca31
NoahKetcham <150949337+NoahKetcham@users.noreply.github.com>
2023-11-27 12:50:14 -0800

    commiting files

---
+++ Basic_HTML_andJavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello world, I am an alert box!");
+function My_First_Function() {
+		var string = "Kiss me, I'm Irish!";
+		var result = string.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+	
